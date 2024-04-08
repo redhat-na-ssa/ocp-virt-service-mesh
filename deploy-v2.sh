0ece593cdb00d51f9dd1e631020d9503857fd045
bugbiteme <leon.s.levy@gmail.com>
2024-04-08 11:36:10 -0700

    finally got weighted routes to work

---
+++ deploy-v2.sh
@@ -0,0 +1,16 @@
+oc apply -f k8/virtualservice-service-b.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-90-10.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-75-25.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-50-50.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-25-75.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-10-90.yaml
+sleep 20
+oc apply -f k8/virtualservice-service-b-0-100.yaml
+
+
+
